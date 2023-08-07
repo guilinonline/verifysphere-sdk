12f71ecb40c817c04ffabfb7216a6138cd05266b
ktxx0027 <chengl@shait.com.cn>
2023-08-07 10:20:38 +0800

    first

--- checkToken_test.go
+++ checkToken_test.go
@@ -27,10 +27,10 @@ func TestLogin(t *testing.T) {
 		Context: string(ctxJSON),
 	}
 	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVSUQiOiJ1LTYyNmU1NTI5YmY1ODAiLCJVc2VybmFtZSI6InRlc3QxIiwiVXNlclR5cGUiOiJub3JtYWwiLCJBdWRpZW5jZSI6InNlbGYiLCJpc3MiOiJWZXJpZnlTcGhlcmUiLCJzdWIiOiJ0ZXN0MSIsImV4cCI6MTY5MTA2MDI5NSwibmJmIjoxNjkxMDMxNDk1LCJpYXQiOjE2OTEwMzE0OTV9.pbA9JjH9djYmf-5gzXVhp3YtwM99z73YnnojWeGx6so"
-	data, code, err := client.CheckToken(vp, token)
+	data, err := client.CheckToken(vp, token)
 	if err != nil {
 		t.Errorf("check failed for  %s", err.Error())
 	}
-	fmt.Println(string(data), code)
+	fmt.Println(data)
 
 }
